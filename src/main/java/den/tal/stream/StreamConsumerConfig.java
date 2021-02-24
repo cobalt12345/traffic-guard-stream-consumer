19242393150cd2a3a78ce62686c7a169056b811b
Denis Talochkin <vgtu@rambler.ru>
2021-02-24 22:06:36 +0300

    Fix config. issues

--- src/main/java/den/tal/stream/StreamConsumerConfig.java
+++ src/main/java/den/tal/stream/StreamConsumerConfig.java
@@ -10,37 +10,43 @@ import com.amazonaws.services.kinesisvideo.AmazonKinesisVideoMediaClientBuilder;
 import com.amazonaws.services.kinesisvideo.model.APIName;
 import com.amazonaws.services.kinesisvideo.model.GetDataEndpointRequest;
 import den.tal.stream.watch.exceptions.FilmWatcherInitException;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
-
 
 @Log4j2
 @Configuration
 @ComponentScan()
 public abstract class StreamConsumerConfig {
 
-    @Autowired
-    private AppConfig appConfig;
+    @Getter
+    @Value("${aws.role_arn}")
+    private String trafficGuardRoleArn;
+
+    @Getter
+    @Value("${aws.region}")
+    private String region;
+
+    @Getter
+    @Value("${kinesis.video.stream.name}")
+    private String videoStreamName;
 
     @Bean
     public STSProfileCredentialsServiceProvider stsCredentialsProvider() {
         RoleInfo roleInfo =
-                new RoleInfo().withRoleArn(appConfig.getTrafficGuardRoleArn())
-                        .withRoleSessionName("traffic-guard-session");
+                new RoleInfo().withRoleArn(trafficGuardRoleArn).withRoleSessionName("traffic-guard-session");
 
         return new STSProfileCredentialsServiceProvider(roleInfo);
     }
 
     @Bean
-    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
         PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
         YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
         yaml.setResources(new ClassPathResource("application.yml"));
@@ -52,7 +58,7 @@ public abstract class StreamConsumerConfig {
     @Bean
     public AmazonKinesisVideo kinesisVideo(STSProfileCredentialsServiceProvider stsCredentialsProvider) {
         AmazonKinesisVideoClientBuilder videoClientBuilder = AmazonKinesisVideoClientBuilder.standard();
-        AmazonKinesisVideo amazonKinesisVideo = videoClientBuilder.withRegion(appConfig.getRegion())
+        AmazonKinesisVideo amazonKinesisVideo = videoClientBuilder.withRegion(region)
                 .withCredentials(stsCredentialsProvider).build();
 
         return amazonKinesisVideo;
@@ -63,8 +69,7 @@ public abstract class StreamConsumerConfig {
                                                      AmazonKinesisVideo kinesisVideo) throws FilmWatcherInitException {
 
         String endpoint = kinesisVideo.getDataEndpoint(new GetDataEndpointRequest()
-                .withAPIName(APIName.GET_MEDIA).withStreamName(appConfig.getVideoStreamName()))
-                    .getDataEndpoint();
+                .withAPIName(APIName.GET_MEDIA).withStreamName(videoStreamName)).getDataEndpoint();
 
         log.debug("Kinesis Video Endpoint: {}", endpoint);
         if (null == endpoint) {
@@ -76,8 +81,7 @@ public abstract class StreamConsumerConfig {
                 AmazonKinesisVideoMediaClientBuilder.standard();
 
         amazonKinesisVideoMediaClientBuilder.withEndpointConfiguration(
-                new AwsClientBuilder.EndpointConfiguration(endpoint, appConfig.getRegion()))
-                    .withCredentials(stsCredentialsProvider);
+                new AwsClientBuilder.EndpointConfiguration(endpoint, region)).withCredentials(stsCredentialsProvider);
 
         return amazonKinesisVideoMediaClientBuilder.build();
     }
