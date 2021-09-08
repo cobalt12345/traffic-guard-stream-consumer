3f360022d00b16499e85b6d7028c755fc9677373
dtalochk <denis.talochkin@gmail.com>
2021-09-08 13:51:55 +0300

    Migration to the new account.

--- scripts/build_and_push_to_aws_ecr.bat
+++ scripts/build_and_push_to_aws_ecr.bat
@@ -1,7 +1,7 @@
 rem Following statements are from the AWS Console for ECS.
 cd .. && ^
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 469694857107.dkr.ecr.eu-central-1.amazonaws.com && ^
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 785668202743.dkr.ecr.eu-central-1.amazonaws.com && ^
 docker build -t traffic-guard-ecr-repo . && ^
-docker tag traffic-guard-ecr-repo:latest 469694857107.dkr.ecr.eu-central-1.amazonaws.com/traffic-guard-ecr-repo:latest && ^
-docker push 469694857107.dkr.ecr.eu-central-1.amazonaws.com/traffic-guard-ecr-repo:latest && ^
+docker tag traffic-guard-ecr-repo:latest 785668202743.dkr.ecr.eu-central-1.amazonaws.com/traffic-guard-ecr-repo:latest && ^
+docker push 785668202743.dkr.ecr.eu-central-1.amazonaws.com/traffic-guard-ecr-repo:latest && ^
 cd scripts
